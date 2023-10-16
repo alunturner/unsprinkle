b4241c632db2d44f8bf925961e63cd0c402a0753
alunturner <alun.turner@googlemail.com>
2023-10-16 13:30:30 +0100

    add alt text where needed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,8 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <HeroImage src="/images/hero-img.jpg" alt="Example of page content"/>
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
